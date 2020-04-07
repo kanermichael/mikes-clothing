6e8f1b3c3e6561f461b72fb64229023c733ee31f
Michael Kaner <michael@Michaels-MacBook-Pro-2.local>
2020-04-07 08:54:42 +0200

    added sign in component

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyB6it64N6jrA6hPvecyGzieKZlTVDFFJus",
+    authDomain: "michael-clothing-db.firebaseapp.com",
+    databaseURL: "https://michael-clothing-db.firebaseio.com",
+    projectId: "michael-clothing-db",
+    storageBucket: "michael-clothing-db.appspot.com",
+    messagingSenderId: "635632977774",
+    appId: "1:635632977774:web:b5ba5c7d8addf2b951637f"
+}
+
+firebase.initializeApp(config)
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+provider.setCustomParameters({prompt: 'select_account'});
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider)
